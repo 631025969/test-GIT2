37cf540e67120bff5a90fbffebb12d948087bef1
631025969 <52305172+631025969@users.noreply.github.com>
2019-06-28 20:46:09 +0800

    Update Fafafs.java

--- src/main/java/Fafafs.java
+++ src/main/java/Fafafs.java
@@ -2,5 +2,6 @@ public class Fafafs {
     public static void main(String[] args) {
         System.out.println("erer");
         System.out.println("dfaf");
+        system。out.println（"new Date()"）;
     }
 }
